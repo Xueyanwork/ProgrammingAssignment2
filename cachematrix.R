7105814464eb7bf191636ceceab673f35944ae30
Jin <xueyanwork789789789@gmail.com>
2016-03-21 01:02:07 +0900

    More typos
    Signed-off-by: Jin <xueyanwork789789789@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(Y){
+    X <<- Y
+    inverse <<- NULL
+  }
+  get <- function() X
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 }
 
@@ -11,5 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse## Return a matrix that is the inverse of 'x'
 }
